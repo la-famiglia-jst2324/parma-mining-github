74d8b5a99def84f173582103973cc7a2740e0282
Constantin Weberpals <constantin.weberpals@tum.de>
2023-11-24 11:23:43 +0100

    add retrieval of repos by organization

--- parma_mining/github/api/main.py
+++ parma_mining/github/api/main.py
@@ -1,8 +1,15 @@
 """Main entrypoint for the API routes in of parma-analytics."""
+from typing import List
 
 from fastapi import FastAPI
 
+from parma_mining.github.client import GitHubClient
+
 app = FastAPI()
+
+# Initialize GitHubClient with your token
+token = "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"  # Replace with your actual GitHub access token
+github_client = GitHubClient(token)
 
 
 # root endpoint
@@ -10,3 +17,17 @@ app = FastAPI()
 def root():
     """Root endpoint for the API."""
     return {"welcome": "at parma-mining-github"}
+
+
+@app.get("/user/{username}", status_code=200)
+def get_user(username: str):
+    """Endpoint to get a GitHub user's information."""
+    user_info = github_client.get_user(username)
+    return {"user_info": user_info}
+
+
+@app.get("/org/{org_name}/repos", response_model=List[str])
+def get_organization_repositories(org_name: str):
+    """Endpoint to get a list of repositories for a given organization."""
+    repos = github_client.list_organization_repositories(org_name)
+    return repos
--- parma_mining/github/client.py
+++ parma_mining/github/client.py
@@ -7,10 +7,17 @@ class GitHubClient:
 
     def get_user(self, username=None):
         try:
-            # Use PyGithub to get the user
-            return (
+            user = (
                 self.client.get_user(username) if username else self.client.get_user()
             )
+            return {
+                "name": user.name,
+                "email": user.email,
+                "bio": user.bio,
+                "public_repos": user.public_repos,
+                "followers": user.followers,
+                "following": user.following,
+            }
         except GithubException as e:
             # Handle exceptions from the GitHub API
             print(f"Error retrieving user: {e}")
@@ -23,4 +30,13 @@ class GitHubClient:
         except GithubException as e:
             # Handle exceptions from the GitHub API
             print(f"Error listing repositories: {e}")
+            return []
+
+    def list_organization_repositories(self, org_name):
+        try:
+            organization = self.client.get_organization(org_name)
+            return [repo.name for repo in organization.get_repos()]
+        except GithubException as e:
+            # Handle exceptions or errors
+            print(f"Error retrieving organization repositories: {e}")
             return []
