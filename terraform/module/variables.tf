fffcff748e204a5f5f11b3731c1c4d1aa4458614
Ege Kocabas <kocabas.ege@tum.de>
2023-11-27 15:54:40 +0100

    add terraform files

---
+++ terraform/module/variables.tf
@@ -0,0 +1,14 @@
+variable "env" {
+  description = "staging or prod environment"
+  type        = string
+}
+
+variable "project" {
+  description = "Google Cloud Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Google Cloud Region"
+  type        = string
+}
