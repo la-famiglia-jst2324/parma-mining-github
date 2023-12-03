e85a0c3f9491b708f131fff0c7c2b4abb6264ea9
Constantin Weberpals <constantin.weberpals@tum.de>
2023-12-03 19:45:06 +0100

    increase robustness

--- parma_mining/github/model.py
+++ parma_mining/github/model.py
@@ -1,40 +1,40 @@
 from datetime import datetime
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 
 class RepositoryModel(BaseModel):
     """Model to structure the JSON Data."""
 
-    name: str
-    description: str
-    stars: int
-    forks: int
-    language: str
-    created_at: datetime
-    updated_at: datetime
-    pushed_at: datetime
-    html_url: str
-    clone_url: str
-    svn_url: str
-    homepage: str
-    size: int
-    stargazers_count: int
-    watchers_count: int
-    forks_count: int
-    open_issues_count: int
+    name: Optional[str]
+    description: Optional[str]
+    stars: Optional[int]
+    forks: Optional[int]
+    language: Optional[str]
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+    pushed_at: Optional[datetime]
+    html_url: Optional[str]
+    clone_url: Optional[str]
+    svn_url: Optional[str]
+    homepage: Optional[str]
+    size: Optional[int]
+    stargazers_count: Optional[int]
+    watchers_count: Optional[int]
+    forks_count: Optional[int]
+    open_issues_count: Optional[int]
 
 
 class OrganizationModel(BaseModel):
-    name: str
-    description: str
-    url: str
-    repos: List[RepositoryModel]
+    name: Optional[str]
+    description: Optional[str]
+    url: Optional[str]
+    repos: Optional[List[RepositoryModel]]
 
 
 class DiscoveryModel(BaseModel):
-    name: str
-    url: str
+    name: Optional[str]
+    url: Optional[str]
 
 
 class CompaniesRequest(BaseModel):
